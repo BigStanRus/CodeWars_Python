458c495b363be6b532c23dce482e5f2faf288d5c
BigStanRus <Klimenko_sasha32@mail.ru>
2021-08-02 17:28:18 +0500

    Create folders

---
+++ 8-kyu/Replace all dots.py
@@ -0,0 +1,9 @@
+"""
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+https://www.codewars.com/kata/596c6eb85b0f515834000049
+"""
+def replace_dots(str):
+    return str.replace('.', '-')
